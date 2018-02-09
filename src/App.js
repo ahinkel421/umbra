089c162a31dd816959420a24e9d1b458646a402c
Adam Hinkel <ahinkel421@gmail.com>
2018-02-09 15:03:10 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import LandingPage from './components/landing-page'
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <LandingPage />
+      </div>
+    );
+  }
+}
---
+++ src/components/landing-page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './../index.css';
+import './landing-page.css'
+
+export default function LandingPage() {
+	return(
+		<h1>Umbra</h1>
+	);
+}
