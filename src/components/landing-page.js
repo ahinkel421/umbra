2377c6dc52b94fef6e52af8cb8bc541d25859216
Adam Hinkel <ahinkel421@gmail.com>
2018-02-16 12:01:37 -0500

    added startGame function

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -3,13 +3,30 @@ import './../index.css';
 import './landing-page.css'
 
 export default class LandingPage extends React.Component {
-	
+	constructor(props) {
+		super(props);
+		this.state = {
+			startedGame: false
+		}
+	}
+
+	startGame(startedGame) {
+		this.setState({
+			startedGame
+		});
+	}
+
 	render() {
+		if(!this.state.startedGame) {
+			return(
+				<div>
+					<h1>Umbra</h1>
+					<button className='start-button' onClick={() => this.startGame(true)}>Start Game</button>
+				</div>
+			);
+		}
 		return(
-			<div>
-				<h1>Umbra</h1>
-				<button className='start-button'>Start Game</button>
-			</div>
+			<dialogSection />
 		);
 	}
 }
