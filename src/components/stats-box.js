26fdf39bd4d5c2bc6213797ea3c0b6d1031091e8
Adam Hinkel <ahinkel421@gmail.com>
2018-02-19 12:25:56 -0500

    stats list

--- src/components/stats-box.js
+++ src/components/stats-box.js
@@ -2,5 +2,26 @@ import React from 'react';
 import Stat from './stat';
 
 export default class StatsBox extends React.Component {
-	
+
+	render() {
+		return(
+			<div className='stats-box'>
+				<h2>Players name</h2>
+				<ul>
+					<li>
+						<Stat />
+					</li>
+					<li>
+						<Stat />
+					</li>
+					<li>
+						<Stat />
+					</li>
+					<li>
+						<Stat />
+					</li>
+				</ul>
+			</div>
+		);
+	}
 }
