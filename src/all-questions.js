abff79b53d93fcfdd7c0c681258e24e8668c1d77
Adam Hinkel <ahinkel421@gmail.com>
2018-03-06 13:52:18 -0500

    refactor

--- src/all-questions.js
+++ src/all-questions.js
@@ -1,7 +1,8 @@
 const QUESTIONS = [
 	{
 		number: 0,
-		text: "Hello traveller. Don't believe I've seen you around here before. What's your name?"
+		text:
+			"Hello traveller. Don't believe I've seen you around here before. What's your name?"
 	}
 ];
 
