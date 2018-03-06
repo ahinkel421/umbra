3cbbd45309c2c3ddb5e1752878efe2c7cb2be8e5
Adam Hinkel <ahinkel421@gmail.com>
2018-03-06 13:42:03 -0500

    added prettierjs

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import DialogSection from './dialog-section';
-import StatsBox from './stats-box';
-import './landing-page.css';
-import QUESTIONS from '../all-questions';
+import React from "react";
+import DialogSection from "./dialog-section";
+import StatsBox from "./stats-box";
+import "./landing-page.css";
+import QUESTIONS from "../all-questions";
 
 export default class LandingPage extends React.Component {
 	constructor(props) {
@@ -10,11 +10,11 @@ export default class LandingPage extends React.Component {
 		this.state = {
 			startedGame: false,
 			currentQuestion: 0,
-			name: '',
+			name: "",
 			strength: 0,
 			speed: 0,
 			intelligence: 0
-		}
+		};
 		this.setName = this.setName.bind(this);
 	}
 
@@ -36,28 +36,29 @@ export default class LandingPage extends React.Component {
 
 	render() {
 		let currentQuestionText = QUESTIONS[this.state.currentQuestion].text;
-		if(!this.state.startedGame) {
-			return(
-				<div className='button-box'>
-					<button onClick={() => this.startGame(true)}>Start Game</button>
+		if (!this.state.startedGame) {
+			return (
+				<div className="button-box">
+					<button onClick={() => this.startGame(true)}>
+						Start Game
+					</button>
 				</div>
 			);
-		}
-		else if(this.state.currentQuestion >= 0) {
-			return(
+		} else if (this.state.currentQuestion >= 0) {
+			return (
 				<div>
 					<DialogSection currentQuestion={currentQuestionText} />
-					<form className='name-form' onSubmit={this.handleSubmit}>
-						<input 
-							className='player-name-input' 
-							placeholder='Enter your name...' 
-							value={this.state.value} 
-							onChange={this.setName} 
+					<form className="name-form" onSubmit={this.handleSubmit}>
+						<input
+							className="player-name-input"
+							placeholder="Enter your name..."
+							value={this.state.value}
+							onChange={this.setName}
 						/>
 						<button className="confirm-name-button">Submit</button>
 					</form>
-					<StatsBox 
-						name={this.state.name} 
+					<StatsBox
+						name={this.state.name}
 						strength={this.state.strength}
 						speed={this.state.speed}
 						intelligence={this.state.intelligence}
