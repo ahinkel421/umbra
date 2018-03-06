452c8b27e13df70929392fc7e9ba270945daffa6
Adam Hinkel <ahinkel421@gmail.com>
2018-03-06 17:20:14 -0500

    added question 0 to QUESTIONS object

--- src/all-questions.js
+++ src/all-questions.js
@@ -1,8 +1,57 @@
+//“Pulvis et umbra sumus. (We are but dust and shadow.)” -Horace (fades in and then out)
+//Umbra fades in, then "start game" button fades in.
+
 const QUESTIONS = [
+	//  Question 0
 	{
 		number: 0,
 		text:
-			"Hello traveller. Don't believe I've seen you around here before. What's your name?"
+			"You’re walking along a candlelit, cobblestone street. Can’t remember how you got there, or where you’re going. You suddenly feel something sharp pressed against your lower back. You hear a voice. “Here’s what’s gunna happen. You’re gunna give me everything you got in those pockets, and then you’re gunna walk away...Got it?” What do you do?",
+		answers: [
+			{
+				text: "Hand it over and keep walking, not worth the trouble.",
+				goToQuestion: 1,
+				statEffect: {
+					strength: 0,
+					speed: 0,
+					intelligence: 3
+				}
+			},
+			{
+				text: "Try for a spinning elbow to knock them out.",
+				goToQuestion: 2,
+				statEffect: {
+					strength: 3,
+					speed: 2,
+					intelligence: 1
+				}
+			},
+			{
+				text:
+					"Pretend to reach for your pocket, grab their wrist instead, attempt to disarm them and run free.",
+				goToQuestion: 3,
+				statEffect: {
+					strength: 1,
+					speed: 3,
+					intelligence: 2
+				}
+			},
+			{
+				text:
+					"Convince them that you have friends in high, watching him as you speak. That he's making a big mistake. (Lie)",
+				goToQuestion: 4,
+				statEffect: {
+					strength: 0,
+					speed: 0,
+					intelligence: 6
+				}
+			}
+		]
+	},
+	//Question 1
+	{
+		number: 1,
+		text: ""
 	}
 ];
 
