4b3e2a00c7774d6f60f7af3662ddf03484996b0f
Adam Hinkel <ahinkel421@gmail.com>
2018-03-09 13:55:02 -0500

    editing form

--- src/components/dialog-section.js
+++ src/components/dialog-section.js
@@ -4,10 +4,26 @@ import Answer from "./answer";
 import "./dialog-section.css";
 
 export default class DialogSection extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			toggled: false,
+			selectedOption: "option0"
+		};
+		this.handleChecked = this.handleChecked.bind(this);
+	}
+
 	handleSubmit(e) {
 		e.preventDefault();
 	}
-	handleChecked(e) {}
+
+	handleChecked() {
+		this.setState(prevState => ({
+			toggled: !prevState.toggled
+		}));
+		console.log(this.state.toggled);
+	}
+
 	render() {
 		return (
 			<section className="dialog-section">
@@ -15,19 +31,44 @@ export default class DialogSection extends React.Component {
 				<form>
 					<ul>
 						<li>
-							<input type="radio" checked="true" />
+							<input
+								type="radio"
+								value="option0"
+								onChange={this.handleChecked}
+								checked={
+									this.state.selectedOption === "option0"
+								}
+							/>
 							<Answer text={this.props.currentAnswers[0].text} />
 						</li>
 						<li>
-							<input type="radio" />
+							<input
+								type="radio"
+								value="option1"
+								checked={
+									this.state.selectedOption === "option1"
+								}
+							/>
 							<Answer text={this.props.currentAnswers[1].text} />
 						</li>
 						<li>
-							<input type="radio" />
+							<input
+								type="radio"
+								value="option2"
+								checked={
+									this.state.selectedOption === "option2"
+								}
+							/>
 							<Answer text={this.props.currentAnswers[2].text} />
 						</li>
 						<li>
-							<input type="radio" />
+							<input
+								type="radio"
+								value="option3"
+								checked={
+									this.state.selectedOption === "option3"
+								}
+							/>
 							<Answer text={this.props.currentAnswers[3].text} />
 						</li>
 					</ul>
