832fb7186cadd7316d75fb32fdcf8ebbdda9a798
Adam Hinkel <ahinkel421@gmail.com>
2018-03-20 17:17:40 -0400

    styling updates

--- src/components/stat.js
+++ src/components/stat.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export default class Stat extends React.Component {
-	constructor(props) {
-		super(props);
-	}
 	render() {
-		return(
-			<li>{this.props.text}: {this.props.value}</li>
+		return (
+			<li>
+				{this.props.text}: {this.props.value}
+			</li>
 		);
 	}
 }
