927e927cd1e3db5724db5febd1a4583391b36fb3
Adam Hinkel <ahinkel421@gmail.com>
2018-04-30 11:21:11 -0400

    adding css animations

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -58,6 +58,7 @@ export default class LandingPage extends React.Component {
 		if (!this.state.startedGame) {
 			return (
 				<div className="button-box">
+					<p className='opening-quote'>“Pulvis et umbra sumus. (We are but dust and shadow.)” -Horace</p>
 					<button className="start-button" onClick={() => this.startGame(true)}>
 						Start Game
 					</button>
